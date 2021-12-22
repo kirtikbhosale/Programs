a8eda2b6d1b21407ea850aca2c1175291c54d3c9
kirtikbhosale <95411787+kirtikbhosale@users.noreply.github.com>
2021-12-22 13:22:19 +0530

    Add files via upload

---
+++ C programs/problems on pattern printing/problems on Pattern printing/Non Linear Pattern Printing/pattern64.c
@@ -0,0 +1,50 @@
+//enter number of rows:4
+//enter number of columns:4
+
+//$ * * *
+//* $ * *
+//* * $ *
+//* * * $
+
+#include<stdio.h>
+void Display(int iRow,int iCol)
+{
+	   int i=0;
+	   int j=0;
+	      
+		for(i=1;i<=iRow;i++)        
+		{
+			
+			for(j=1;j<=iCol;j++)
+		 {
+				if(i==j)
+			{
+			  printf("$\t");
+			}
+			else
+			{
+				printf("*\t");
+			}
+		 }	
+		   printf("\n");
+        }
+}
+
+
+
+int main()
+{
+   int iValue1=0;
+   int iValue2=0;
+   printf("enter number of rows\n");
+   scanf("%d",&iValue1);
+   
+   printf("enter number of columns\n");
+   scanf("%d",&iValue2);
+   Display(iValue1,iValue2);
+  
+   return 0;
+
+
+
+}
